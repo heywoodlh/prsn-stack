eb2b1035943bd418bd20f71a7e4e7b9b34c0b66c
Spencer Heywood <l.spencer.heywood@gmail.com>
2017-12-13 15:52:01 -0700

    added remove network command

--- uninstall.sh
+++ uninstall.sh
@@ -5,3 +5,6 @@ docker rm -f plex radarr sonarr nzbget
 
 echo "removing local images for containers"
 docker image rm linuxserver/plex linuxserver/radarr linuxserver/sonarr linuxserver/nzbget
+
+echo "removing prsnstack network"
+docker network rm prsnstack
