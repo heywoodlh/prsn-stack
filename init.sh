083ea561264222fe2f7a98982b63d9bb749cb21b
Spencer Heywood <l.spencer.heywood@gmail.com>
2017-12-13 11:24:42 -0700

    added all dependant folders

--- init.sh
+++ init.sh
@@ -41,24 +41,16 @@ then
 fi
 
 ##docker-plex build steps
-cd ./docker-plex
 docker create --name=plex --net=host -e VERSION=latest -e PUID="$PUID" -e PGID="$PGID" -e TZ="$TZ" -v "$PLEX_DIR":/config -v "$TV_SHOWS":/data/tvshows -v "$MOVIE_DIR":/data/movies linuxserver/plex
-cd ../
 
 ##docker-couchpotato build steps
-cd ./docker-couchpotato
 docker create --name=couchpotato -v "$COUCHPOTATO_DIR":/config -v "$NZB_DOWNLOADS_DIR":/downloads -v "$MOVIE_DIR":/movies -e PGID="$PGID" -e PUID="$PUID"  -e TZ="$TZ" -p 5050:5050 linuxserver/couchpotato
-cd ../
 
 ##docker-sonarr build steps
-cd ./docker-sonarr
 docker create --name sonarr -p 8989:8989 -e PUID="$PUID" -e PGID="$PGID" -e TZ="$TZ" -v "$SONARR_DIR":/config -v "$TV_SHOWS":/tv -v "$NZB_DOWNLOADS_DIR":/downloads linuxserver/sonarr
-cd ..
 
 ##docker-nzbget build steps
-cd ./docker-nzbget
 docker create --name nzbget -p 6789:6789 -e PUID="$PUID" -e PGID="$PGID" -e TZ="$TZ" -v "$NZBGET_DIR":/config -v "$NZB_DOWNLOADS_DIR":/downloads linuxserver/nzbget
-cd ..
 
 docker start plex
 docker start couchpotato
